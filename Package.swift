dfecd2fee3bd09e304c5e839ff8352b4fc5db144
qxu <antwork@126.com>
2019-09-25 10:09:40 +0800

    update package.swift

--- Package.swift
+++ Package.swift
@@ -1,15 +1,17 @@
-// swift-tools-version:4.0
+// swift-tools-version:5.1
 import PackageDescription
 
 let package = Package(
     name: "SQLite.swift",
     products: [.library(name: "SQLite", targets: ["SQLite"])],
+    dependencies: [
+        .package(url: "git@github.com:antwork/SQLCipher.git", from: "0.0.4")
+    ],
     targets: [
-        .target(name: "SQLite", dependencies: ["SQLiteObjc"]),
+        .target(name: "SQLite", dependencies: ["SQLiteObjc", "SQLCipher"], swiftSettings: [.define("SQLITE_SWIFT_SQLCIPHER")]),
         .target(name: "SQLiteObjc"),
         .testTarget(name: "SQLiteTests", dependencies: ["SQLite"], path: "Tests/SQLiteTests")
-    ],
-    swiftLanguageVersions: [4, 5]
+    ]
 )
 
 #if os(Linux)
